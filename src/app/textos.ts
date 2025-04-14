e72d3581eb5144d4c9118ab665dd019cd711338c
obaiges <oscarbaigesr@gmail.com>
2025-04-14 16:10:43 +0200

    experiencia styles + traducciones

--- src/app/textos.ts
+++ src/app/textos.ts
@@ -19,7 +19,21 @@ export const textos = {
         experience: {
             title: 'Desarrollador Web Full-Stack',
             title2: 'Responsable del Departamento de TI',
-            time: 'Actualmente'
+            time: 'Actualmente',
+            description: 'Actualmente lidero un equipo de 6 programadores en el que estamos en continuo aprendizaje y desarrollo de nuevas tecnologías. Organizo y asumo la responsabilidad de los proyectos y la coordinación del equipo, sin olvidar mis tareas diarias como programador.',
+            pastillas: {
+                past1: 'Gestión de proyectos',
+                past2: 'Conocimientos de empresa',
+                past3: 'Automatización de procesos',
+                past4: 'Y muchas cosas más...'
+            },
+            description2: 'Mis labores fueron desarrollar, desplegar y mantener un sistema ERP que permite la automatización de procesos, la gestión de proyectos y la coordinación de equipos, aumentando el rendimiento un 50%. Además, hice varias landing pages para adquirir habilidades en el desarrollo de sitios web responsivos.',
+            pastillas2: {
+                past1: 'Desarrollo Front-End y Back-End',
+                past2: 'Creación de servidores',
+                past3: 'Organización y buenos hábitos',
+                past4: 'Migraciones'
+            }
         }
     },
     en: {
@@ -40,7 +54,21 @@ export const textos = {
         experience: {
             title: 'Full-Stack Web Developer',
             title2: 'Head of IT Department',
-            time: 'Now'
+            time: 'Now',
+            description: 'I currently lead a team of 6 programmers in which we are continuously learning and developing new technologies. I organise and take responsibility for the projects and the coordination of the team, without forgetting my daily tasks as a programmer.',
+            pastillas: {
+                past1: 'Project management',
+                past2: 'Corporate knowledge',
+                past3: 'Process automation',
+                past4: 'And many more...'
+            },
+            description2: 'My tasks were developing, deploying and maintaining an ERP system that allows process automation, project management and team coordination, increasing performance by 50%. In addition, I made several landing pages to acquire skills in responsive website development.', 
+            pastillas2: {
+                past1: 'Front-End and Back-End development',
+                past2: 'Server creation',
+                past3: 'Organization and good habits',
+                past4: 'Migrations'
+            }
         }
     }
 }
